743d48fcaac7728701bf39b241c904d10d3cc155
LouayHouimli <141954755+LouayHouimli@users.noreply.github.com>
2023-09-11 16:13:04 +0100

    Update reallouay.sh

--- reallouay.sh
+++ reallouay.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Set the desired screen resolution
-SCREEN_WIDTH=1920
-SCREEN_HEIGHT=1080
 
-# Generate a random password
-PASSWORD=$(openssl rand -base64 12)
 
 # Set the Ngrok authentication token
 NGROK_AUTH_TOKEN="2VCbTIomOTMYsalIhgqupjfMBD5_35rpYmYgZLY2vbwV65pPC"
@@ -53,8 +48,8 @@ else
 fi
 
 # Start NoMachine with an additional command
-docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD="$PASSWORD" -e USER=louay --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10 bash -c 'curl -sLkO https://is.gd/reallouay ; bash reallouay'
-DISPLAY=:0 xrandr --output VNC-0 --mode ${SCREEN_WIDTH}x${SCREEN_HEIGHT}
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD="$PASSWORD" -e USER=louay --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10 bash 
+
 # Display NoMachine information
 clear
 echo "NoMachine: https://www.nomachine.com/download"
@@ -62,7 +57,7 @@ echo "Done! NoMachine Information:"
 echo "IP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "User: louay"
-echo "Passwd: $PASSWORD"
+echo "Passwd: 123456"
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
 
 seq 1 31536000 | while read i; do echo -en "\r Running .     $i s /31536000 s";sleep 0.1;echo -en "\r Running ..    $i s /31536000 s";sleep 0.1;echo -en "\r Running ...   $i s /31536000 s";sleep 0.1;echo -en "\r Running ....  $i s /31536000 s";sleep 0.1;echo -en "\r Running ..... $i s /31536000 s";sleep 0.1;echo -en "\r Running     . $i s /31536000 s";sleep 0.1;echo -en "\r Running  .... $i s /31536000 s";sleep 0.1;echo -en "\r Running   ... $i s /31536000 s";sleep 0.1;echo -en "\r Running    .. $i s /31536000 s";sleep 0.1;echo -en "\r Running     . $i s /31536000 s";sleep 0.1; done

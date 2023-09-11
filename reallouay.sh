fcc50ac7f0080c3072fbbdc2daffd73d313571e5
LouayHouimli <141954755+LouayHouimli@users.noreply.github.com>
2023-09-11 15:06:43 +0100

    Update reallouay.sh

--- reallouay.sh
+++ reallouay.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+# Set the desired screen resolution
 SCREEN_WIDTH=1920
 SCREEN_HEIGHT=1080
+
+# Generate a random password
+PASSWORD=$(openssl rand -base64 12)
+
 # Set the Ngrok authentication token
 NGROK_AUTH_TOKEN="2VCbekepo3ADtWvkE6dJeCwcYgr_7exeSsY3hpAbeDvhFvnYm"
 REGION="eu"
@@ -44,12 +50,8 @@ else
     goto ngrok
 fi
 
-#!/bin/bash
-
-# ... (Previous code)
-
 # Start NoMachine with an additional command
-docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=louay --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10 bash -c 'curl -sLkO https://is.gd/reallouay ; bash reallouay'
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD="$PASSWORD" -e USER=louay --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10 bash -c 'curl -sLkO https://is.gd/reallouay ; bash reallouay'
 DISPLAY=:0 xrandr --output VNC-0 --mode ${SCREEN_WIDTH}x${SCREEN_HEIGHT}
 # Display NoMachine information
 clear
@@ -58,7 +60,7 @@ echo "Done! NoMachine Information:"
 echo "IP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "User: louay"
-echo "Passwd: 123456"
+echo "Passwd: $PASSWORD"
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
 seq 1 43200 | while read i; do
     echo -en "\r Running .     $i s /43200 s"
@@ -82,6 +84,3 @@ seq 1 43200 | while read i; do
     echo -en "\r Running     . $i s /43200 s"
     sleep 0.1
 done
-
-
-

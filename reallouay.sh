adb8552a0001cfef1a979ced07f3870f6eb4ef74
LouayHouimli <141954755+LouayHouimli@users.noreply.github.com>
2023-09-12 17:29:16 +0100

    Update reallouay.sh

--- reallouay.sh
+++ reallouay.sh
@@ -62,26 +62,26 @@ while true; do
     echo "User: louay"
     echo "Passwd: 123456"
     echo "VM can't connect? Restart Cloud Shell then Re-run script."
-seq 1 43200 | while read i; do
-    echo -en "\r Running .     $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running ..    $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running ...   $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running ....  $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running ..... $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running     . $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running  .... $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running   ... $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running    .. $i s /43200 s"
-    sleep 0.1
-    echo -en "\r Running     . $i s /43200 s"
-    sleep 0.1
-done
+    seq 1 43200 | while read i; do
+        echo -en "\r Running .     $i s /43200 s"
+        sleep 0.1
+        echo -en "\r Running ..    $i s /43200 s"
+        sleep 0.1
+        echo -en "\r Running ...   $i s /43200 s"
+        sleep 0.1
+        echo -en "\r Running ....  $i s /43200 s"
+        sleep 0.1
+        echo -en "\r Running ..... $i s /43200 s"
+        sleep 0.1
+        echo -en "\r Running     . $i s /43200 s"
+        sleep 0.1
+        echo -en "\r Running  .... $i s /43200 s"
+        sleep 0.1
+        echo -en "\r Running   ... $i s /43200 s"
+        sleep 0.1
+        echo -en "\r Running    .. $i s /43200 s"
+        sleep 0.1
+        echo -en "\r Running     . $i s /43200 s"
+        sleep 0.1
+    done
 
